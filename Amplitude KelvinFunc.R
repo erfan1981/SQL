905577b849893609c9c18dbdf4d596e283f6a9bc
erfan1981 <34257315+erfan1981@users.noreply.github.com>
2017-12-05 14:08:19 -0800

    Update and rename A KelvinFunc.R to Amplitude KelvinFunc.R

--- Amplitude KelvinFunc.R
+++ Amplitude KelvinFunc.R
@@ -1,16 +1,16 @@
+# Solution to Equ. 64  to 68 of https://pubs.usgs.gov/wri/wri034267/wri03_4267.pdf
 library(kelvin)
 library(rootSolve)
 s = 0.0001 #storage coefficient
-rw = 0.08333333 # in ft
-rc = 0.33333333 # in ft
+rw = 0.08333333 # well radius in ft
+rc = 0.33333333 # well casing in ft
 A = 0.0057
-SP = s*(rc/rw)^2
-Per = 4.47E+04 #x is T'
+SP = s*(rc/rw)^2 # S'
+Per = 4.47E+04 # x is T' and Per is period
 fun <- function (x) (A-(((1-pi/x*Kei((2*pi*SP/x)^0.5)))^2+(pi/x*(Ker((2*pi*SP/x)^0.5))^2))^(-0.5))
 curve(fun(x), 0.005, 0.2)
 abline(h = 0, lty = 3)
 TP <- uniroot(fun, c(0.005, 0.2), tol =0.000000000000000000001)
 #fun(all)
 Tr = TP$root*(rc^2)/Per #Transmissivity
-Tr
-	
+Tr	
